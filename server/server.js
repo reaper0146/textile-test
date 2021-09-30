0e5318d544c7eba60cd8248498d738d7dff2b66c
soumyapal96 <pal.soumya1996@gmail.com>
2021-09-30 15:12:25 -0400

    update

--- server/server.js
+++ server/server.js
@@ -1,24 +1,64 @@
 const express=require('express');
 const mysql = require('mysql');
 const cors=require('cors');
+const IPFS = require('ipfs')
+const OrbitDB = require('orbit-db')
+const Identities = require('orbit-db-identity-provider')
 //const { Leth, Web3, Gateway} = require('lightstreams-js-sdk')
 
 const app=express();
 app.use(express.json())
 app.use(cors())
 
-const { Gateway }  = require('lightstreams-js-sdk')
-const gateway = Gateway('https://gateway.sirius.lightstreams.io')
+const initIPFSInstance = async () => {
+    console.log('hoi')
+    return await IPFS.create({ repo: "./path-for-js-ipfs-repo" });
+  };
+  
+  initIPFSInstance().then(async ipfs => {
 
-async function init() {
-    //const account = "0xa981f8ca77d069d79b609ca0069b052db79e7e30"
-    //const file = fs.createReadStream(`/tmp/my_secret_file.txt`)
-    //const { meta, acl } = await gateway.storage.add(account, "password", file)
-    // Get user balance
-    const account = "0xa981f8ca77d069d79b609ca0069b052db79e7e30"
-    const { balance } = await gateway.wallet.balance(account)
-    console.log(balance)
-}
+    const ipfsOptions = {
+        EXPERIMENTAL: {
+          pubsub: true
+        }
+      }
+
+    const ipfs = await IPFS.create(ipfsOptions)  
+    //const orbitdb = await OrbitDB.createInstance(ipfs);
+  
+    // Create / Open a database
+    //const db = await orbitdb.keyvalue('test-db')
+    //await db.load();
+
+    //const db = orbitDB.keyvalue('test-db')
+    //console.log(db.address.toString())
+
+    const identity = await Identities.createIdentity(options)
+    console.log(identity.toJSON())
+    const orbitdb = await OrbitDB.createInstance(ipfs,  { identity: identity })
+ 
+    const db = orbitDB.keyvalue('test-db')
+    await db.put({ _id: 'test', name: 'test-doc-db', category: 'distributed' })
+
+    const address = db.address.toString()
+    console.log(address)
+    await db2.load()
+    const value = db2.get()
+    console.log(value)
+    // Listen for updates from peers
+    //db.events.on("replicated", address => {
+    //  console.log(db.iterator({ limit: -1 }).collect());
+    //});
+  
+    // Add an entry
+    //const hash = await db.add("world");
+    //console.log(hash);
+  
+    // Query
+    //const result = db.iterator({ limit: -1 }).collect();
+    //console.log(JSON.stringify(result, null, 2));
+  });
+  
 
 const db = mysql.createConnection({
     user:"root",
@@ -32,7 +72,7 @@ app.post('/register', (req,res)=> {
     const password = req.body.password
     console.log(username)
     console.log(password)
-    init()
+    initIPFSInstance()
 
     /*db.query("INSERT INTO users (username, password) VALUES (?,?)", [username, password], 
     (err,result) => {console.log(err);}
